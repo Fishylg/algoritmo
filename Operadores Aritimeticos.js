aae02442dff8e45b7196360dd9d5a91762669e0d
Fishy <ryanlimagermano@gmail.com>
2024-05-23 11:57:29 -0300

    Aula sobre operadores aritméticos

---
+++ Operadores Aritimeticos.js
@@ -0,0 +1,26 @@
+var NumeroMultiplicado = 6
+var n0 = NumeroMultiplicado * 0
+var n1 = NumeroMultiplicado * 1
+var n2 = NumeroMultiplicado * 2
+var n3 = NumeroMultiplicado * 3
+var n4 = NumeroMultiplicado * 4
+var n5 = NumeroMultiplicado * 5
+var n6 = NumeroMultiplicado * 6
+var n7 = NumeroMultiplicado * 7
+var n8 = NumeroMultiplicado * 8
+var n9 = NumeroMultiplicado * 9
+var n10 = NumeroMultiplicado * 10
+
+console.log ("_______________")
+console.log ("|", NumeroMultiplicado, "X 0 =", n0, "  |")
+console.log ("|", NumeroMultiplicado, "X 1 =", n1, "  |")
+console.log ("|", NumeroMultiplicado, "X 2 =", n2, " |")
+console.log ("|", NumeroMultiplicado, "X 3 =", n3, " |")
+console.log ("|", NumeroMultiplicado, "X 4 =", n4, " |")
+console.log ("|", NumeroMultiplicado, "X 5 =", n5, " |")
+console.log ("|", NumeroMultiplicado, "X 6 =", n6, " |")
+console.log ("|", NumeroMultiplicado, "X 7 =", n7, " |")
+console.log ("|", NumeroMultiplicado, "X 8 =", n8, " |")
+console.log ("|", NumeroMultiplicado, "X 9 =", n9, " |")
+console.log ("|", NumeroMultiplicado, "X 10 =", n10, "|")
+console.log ("_______________")
